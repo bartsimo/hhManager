df697d1187abc3ecea0d3f98db2b5d18c821002b
bartsimo <bartke.simon@gmail.com>
2022-04-03 14:25:11 +0200

    Added String to Int converter in LeseCSV

--- hhManager/src/hhManager/LeseCSV.java
+++ hhManager/src/hhManager/LeseCSV.java
@@ -23,15 +23,43 @@ public class LeseCSV {
     /*for(int i = 0; i < parts.length; i++) {   
     	System.out.println(parts[i]);    
     	}
-    */
+    
     System.out.println(parts[0]);
     System.out.println(parts[1]);
     System.out.println(parts[2]);
     System.out.println(parts[3]);
     System.out.println(parts[4]);
+    
+    */
+    
+    int betrag = convert(parts[0]);
+    System.out.println("Integer value = " + betrag);
     }   
     sc.close();  //closes the scanner  
-    }  
+    
+
+    
+    }
+    
+
+    
+    public static int convert(String str)
+    {
+        int val = 0;
+        System.out.println("String = " + str);
+  
+        // Convert the String
+        try {
+            val = Integer.parseInt(str);
+        }
+        catch (NumberFormatException e) {
+  
+            // This is thrown when the String
+            // contains characters other than digits
+            System.out.println("Invalid String");
+        }
+        return val;
+    }
 
 
 }
